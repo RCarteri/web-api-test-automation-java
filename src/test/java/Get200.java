da037dc7f1544f8d5cd18303a930efa3f4333bd1
C1324634 Ricardo Carteri Uehlein <c1324634@interno.bb.com.br>
2022-07-13 10:00:16 -0300

    5 - Test setup and teardown

--- src/test/java/Get200.java
+++ src/test/java/Get200.java
@@ -1,9 +1,52 @@
+import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import java.io.IOException;
+
+import static org.testng.Assert.assertEquals;
+
 public class Get200 {
+    public static final String BASE_ENDPOINT = "http://api.github.com";
+    CloseableHttpClient client;
+    CloseableHttpResponse response;
+
+    @BeforeMethod
+    public void setUp(){
+        client = HttpClientBuilder.create().build();
+    }
+
+    @AfterMethod
+    public void closeResources() throws IOException {
+        client.close();
+        response.close();
+    }
+
     @Test
-    public void baseUrlReturn200(){
+    public void baseUrlReturn200() throws IOException {
         HttpGet get = new HttpGet("https://api.github.com");
+        response = client.execute(get);
+        int status = response.getStatusLine().getStatusCode();
+        assertEquals(status, 200);
+    }
+
+    @Test
+    public void rateLimitReturns200() throws IOException {
+        HttpGet get = new HttpGet(BASE_ENDPOINT + "/rate_limit");
+        response = client.execute(get);
+        int status = response.getStatusLine().getStatusCode();
+        assertEquals(status, 200);
+    }
+
+    @Test
+    public void searchReposReturns200() throws IOException {
+        HttpGet get = new HttpGet(BASE_ENDPOINT + "/search/repositories?q=java");
+        response = client.execute(get);
+        int status = response.getStatusLine().getStatusCode();
+        assertEquals(status, 200);
     }
 }
