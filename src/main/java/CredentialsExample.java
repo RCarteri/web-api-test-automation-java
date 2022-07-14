88633167cebba0c5ebe6777eccada7330b016847
C1324634 Ricardo Carteri Uehlein <c1324634@interno.bb.com.br>
2022-07-14 11:25:50 -0300

    7 - Sending a POST request

---
+++ src/main/java/CredentialsExample.java
@@ -0,0 +1,6 @@
+
+public class CredentialsExample {
+    public static String TOKEN = "your real token here";
+    public static String EMAIL = "your@email.com";
+    public static String PASSWORD = "yourPassword";
+}
